a07da0d543cac2e88d3c6c54a3a6ac4ed5b6c7d2
iakrevetkho <40871973+iakrevetkho@users.noreply.github.com>
2018-07-28 23:26:46 +0300

    Update deploy.sh
    Add auto-yes to all installations

--- deploy.sh
+++ deploy.sh
@@ -14,10 +14,10 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 #Update the apt package index
-sudo apt-get update
+sudo apt-get update -y
 
 # Install docker-ce:
-sudo apt-get install docker-ce
+sudo apt-get install docker-ce -y
 
 # Install docker-compose
 # Download
